4e38bacf38fde9499a0d3ab6c318f85d7ac68276
Андрей Рябченко <ryabchenko.a@delcomp.ru>
2021-10-29 18:02:06 +0700

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,10 @@
+import '../styles/main.scss'
+
+window.$ = window.jQuery = require('jquery');
+// import * as $ from 'jquery';
+import 'bootstrap';
+// require("@fancyapps/fancybox");
+// require('./filter.js');
+// require('./forms.js');
+
+
